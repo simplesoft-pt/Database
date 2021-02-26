64390b6f427bb57c4cd886373178ec6a3b8d243c
João Simões <gravity00@users.noreply.github.com>
2021-02-26 17:35:23 +0000

    interface for entities with external identifiers

---
+++ src/Simplesoft.Database.Contracts/IHaveExternalId.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents an entity with an unique identifier to be used
+    /// across multiple systems
+    /// </summary>
+    /// <typeparam name="TId">The external id type</typeparam>
+    public interface IHaveExternalId<TId> where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        TId ExternalId { get; set; }
+    }
+    /// <summary>
+    /// Represents an entity with an unique identifier to be used
+    /// across multiple systems, with a <see cref="Guid"/> for the
+    /// <see cref="IHaveExternalId{TId}.ExternalId"/> property.
+    /// </summary>
+    public interface IHaveExternalId : IHaveExternalId<Guid>
+    {
+
+    }
+}
