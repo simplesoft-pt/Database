ff1be4d3528860cc14b1ca4c82cc961323fcffdd
João Simões <gravity00@users.noreply.github.com>
2021-02-26 17:04:55 +0000

    interface for created metadata

---
+++ src/Simplesoft.Database.Contracts/IHaveCreatedMeta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents an entity with created metadata properties
+    /// </summary>
+    /// <typeparam name="TBy">The created by type</typeparam>
+    public interface IHaveCreatedMeta<TBy>
+    {
+        /// <summary>
+        /// Date and time when the entity was created
+        /// </summary>
+        DateTimeOffset CreatedOn { get; set; }
+
+        /// <summary>
+        /// User that created the entity
+        /// </summary>
+        TBy CreatedBy { get; set; }
+    }
+
+    /// <summary>
+    /// Represents an entity with created metadata properties with
+    /// a string for the created by property
+    /// </summary>
+    public interface IHaveCreatedMeta : IHaveCreatedMeta<string>
+    {
+
+    }
+}
