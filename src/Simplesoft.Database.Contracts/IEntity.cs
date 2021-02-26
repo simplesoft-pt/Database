ca15201a95ef1daddf04dc3802aa30e478852a3a
João Simões <gravity00@users.noreply.github.com>
2021-02-26 16:49:22 +0000

    interface representing entities

---
+++ src/Simplesoft.Database.Contracts/IEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents a database entity with the property
+    /// <see cref="Id"/> representing its unique identifier
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    public interface IEntity<TId> : IEntity where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        TId Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a database entity
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+}
