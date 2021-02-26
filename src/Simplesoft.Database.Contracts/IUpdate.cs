d554f9bc1341de4cecf35968524099d10d238b9e
João Simões <gravity00@users.noreply.github.com>
2021-02-26 20:06:42 +0000

    interface representing the update operation

---
+++ src/Simplesoft.Database.Contracts/IUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the update operation
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IUpdate<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        /// Updates an entity
+        /// </summary>
+        /// <param name="entity">The entity to update</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The entity after changes</returns>
+        Task<TEntity> UpdateAsync(TEntity entity, CancellationToken ct);
+    }
+}
---
+++ src/Simplesoft.Database.Contracts/IUpdateRange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the update operation in bulk
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IUpdateRange<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        /// Updates a range of entities
+        /// </summary>
+        /// <param name="entities">The entity collection</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The entity collection after changes</returns>
+        Task<IEnumerable<TEntity>> UpdateAsync(IEnumerable<TEntity> entities, CancellationToken ct);
+
+        /// <summary>
+        /// Updates a range of entities
+        /// </summary>
+        /// <param name="ct">The cancellation token</param>
+        /// <param name="entities">The entity collection</param>
+        /// <returns>The entity collection after changes</returns>
+        Task<IEnumerable<TEntity>> UpdateAsync(CancellationToken ct, params TEntity[] entities);
+    }
+}
