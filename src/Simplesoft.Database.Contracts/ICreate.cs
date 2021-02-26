2fb9a3460e3b3a6ae00b96181f36cea97ffe62cb
João Simões <gravity00@users.noreply.github.com>
2021-02-26 18:31:09 +0000

    interface representing the create operation

---
+++ src/Simplesoft.Database.Contracts/ICreate.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the create operation
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface ICreate<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        /// Creates an entity
+        /// </summary>
+        /// <param name="entity">The entity to create</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The entity after changes</returns>
+        Task<TEntity> CreateAsync(TEntity entity, CancellationToken ct);
+    }
+}
---
+++ src/Simplesoft.Database.Contracts/ICreateRange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the create operation in bulk
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface ICreateRange<TEntity> where TEntity : IEntity
+    {
+        /// <summary>
+        /// Creates a range of entities
+        /// </summary>
+        /// <param name="entities">The entity collection</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The entity collection after changes</returns>
+        Task<IEnumerable<TEntity>> CreateRangeAsync(IEnumerable<TEntity> entities, CancellationToken ct);
+
+        /// <summary>
+        /// Creates a range of entities
+        /// </summary>
+        /// <param name="ct">The cancellation token</param>
+        /// <param name="entities">The entity collection</param>
+        /// <returns>The entity collection after changes</returns>
+        Task<IEnumerable<TEntity>> CreateRangeAsync(CancellationToken ct, params TEntity[] entities);
+    }
+}
