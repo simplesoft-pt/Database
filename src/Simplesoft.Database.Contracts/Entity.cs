03524257c3cd92dd591c5c899a42491d52a3edaf
João Simões <gravity00@users.noreply.github.com>
2021-02-26 16:55:45 +0000

    abstract entity classes

---
+++ src/Simplesoft.Database.Contracts/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents a database entity with the property
+    /// <see cref="Id"/> representing its unique identifier
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    public abstract class Entity<TId> : IEntity<TId> where TId : IEquatable<TId>
+    {
+        /// <inheritdoc />
+        public virtual TId Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a database entity with a long
+    /// representing the unique identifier
+    /// </summary>
+    public abstract class Entity : IEntity
+    {
+        /// <inheritdoc />
+        public virtual long Id { get; set; }
+    }
+}
