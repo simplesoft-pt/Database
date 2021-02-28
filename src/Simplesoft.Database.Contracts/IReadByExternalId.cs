2211ec2ce0013e190bc2da3a05b807af959ec29d
João Simões <gravity00@users.noreply.github.com>
2021-02-28 02:26:09 +0000

    interface for read operations using external ids

---
+++ src/Simplesoft.Database.Contracts/IReadByExternalId.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the read operation by an external unique identifier
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public interface IReadByExternalId<TEntity, in TId> 
+        where TEntity : IEntity, IHaveExternalId<TId> 
+        where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Read an entity by a unique identifier, returning null if not found.
+        /// </summary>
+        /// <param name="id">The entity id</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The entity or null if not found</returns>
+        Task<TEntity> ReadByExternalIdAsync(TId id, CancellationToken ct);
+    }
+
+    /// <summary>
+    /// Represents the read operation by an external unique identifier
+    /// of <see cref="Guid"/> type.
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IReadByExternalId<TEntity> : IReadByExternalId<TEntity, Guid>
+        where TEntity : IEntity, IHaveExternalId
+    {
+
+    }
+}
---
+++ src/Simplesoft.Database.Contracts/IReadByExternalIdRange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the read bulk operation by a collection of
+    /// external unique identifiers
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public interface IReadByExternalIdRange<TEntity, in TId> 
+        where TEntity : IEntity, IHaveExternalId<TId>
+        where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Reads a collection of entities by their external unique identifiers.
+        /// </summary>
+        /// <param name="ids">The collection of ids</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The collection of entities</returns>
+        Task<IEnumerable<TEntity>> ReadByExternalIdAsync(IEnumerable<TId> ids, CancellationToken ct);
+
+        /// <summary>
+        /// Reads a collection of entities by their external unique identifiers.
+        /// </summary>
+        /// <param name="ct">The cancellation token</param>
+        /// <param name="ids">The collection of ids</param>
+        /// <returns>The collection of entities</returns>
+        Task<IEnumerable<TEntity>> ReadByExternalIdAsync(CancellationToken ct, params TId[] ids);
+    }
+
+    /// <summary>
+    /// Represents the read bulk operation by a collection of
+    /// external unique identifiers of <see cref="Guid"/> type.
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IReadByExternalIdRange<TEntity> : IReadByExternalIdRange<TEntity, Guid>
+        where TEntity : IEntity, IHaveExternalId
+    {
+
+    }
+}
