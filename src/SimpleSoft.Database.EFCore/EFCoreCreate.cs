1ffa1d335476663407a99fa54442ea30b149714c
João Simões <gravity00@users.noreply.github.com>
2021-02-28 03:29:29 +0000

    implementation of the create operations

---
+++ src/SimpleSoft.Database.EFCore/EFCoreCreate.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the create operation
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public class EFCoreCreate<TContext, TEntity> : ICreate<TEntity>
+        where TContext : DbContext
+        where TEntity : class, IEntity
+    {
+        private readonly TContext _context;
+        private readonly DbSet<TEntity> _set;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context"></param>
+        public EFCoreCreate(
+            TContext context
+        )
+        {
+            _context = context;
+            _set = context.Set<TEntity>();
+        }
+
+        /// <inheritdoc />
+        public async Task<TEntity> CreateAsync(TEntity entity, CancellationToken ct)
+        {
+            var entry = await _set.AddAsync(entity, ct);
+            await _context.SaveChangesAsync(ct);
+
+            return entry.Entity;
+        }
+    }
+}
---
+++ src/SimpleSoft.Database.EFCore/EFCoreCreateRange.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the create operation in bulk
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    public class EFCoreCreateRange<TContext, TEntity> : ICreateRange<TEntity>
+        where TContext : DbContext
+        where TEntity : class, IEntity
+    {
+        private readonly TContext _context;
+        private readonly DbSet<TEntity> _set;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context"></param>
+        public EFCoreCreateRange(
+            TContext context
+        )
+        {
+            _context = context;
+            _set = context.Set<TEntity>();
+        }
+
+        /// <inheritdoc />
+        public Task<IEnumerable<TEntity>> CreateAsync(IEnumerable<TEntity> entities, CancellationToken ct) =>
+            CreateAsync(entities as IList<TEntity> ?? entities.ToList(), ct);
+
+        /// <inheritdoc />
+        public Task<IEnumerable<TEntity>> CreateAsync(CancellationToken ct, params TEntity[] entities) =>
+            CreateAsync(entities, ct);
+
+        private async Task<IEnumerable<TEntity>> CreateAsync(IList<TEntity> entities, CancellationToken ct)
+        {
+            await _set.AddRangeAsync(entities, ct);
+            await _context.SaveChangesAsync(ct);
+
+            return entities;
+        }
+    }
+}
