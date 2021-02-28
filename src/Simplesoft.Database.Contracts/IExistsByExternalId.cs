6011bb0af8f04c5089d4fd9d85cfbc00a353a4ce
João Simões <gravity00@users.noreply.github.com>
2021-02-28 03:17:51 +0000

    interface representing the exists operation

---
+++ src/Simplesoft.Database.Contracts/IExistsByExternalId.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the exists operation by an external unique identifier
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public interface IExistsByExternalId<TEntity, in TId>
+        where TEntity : class, IEntity, IHaveExternalId<TId>
+        where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Checks if an entity exists for a given unique identifier
+        /// </summary>
+        /// <param name="id">The entity id</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>True if exists, otherwise false</returns>
+        Task<bool> ExistsAsync(TId id, CancellationToken ct);
+    }
+
+    /// <summary>
+    /// Represents the exists operation by an external unique identifier
+    /// of <see cref="Guid"/> type.
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IExistsByExternalId<TEntity> : IExistsByExternalId<TEntity, Guid>
+        where TEntity : class, IEntity, IHaveExternalId
+    {
+
+    }
+}
---
+++ src/Simplesoft.Database.Contracts/IExistsById.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the exists operation by an unique identifier.
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public interface IExistsById<TEntity, in TId>
+        where TEntity : class, IEntity<TId>
+        where TId : IEquatable<TId>
+    {
+        /// <summary>
+        /// Checks if an entity exists for a given unique identifier
+        /// </summary>
+        /// <param name="id">The entity id</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>True if exists, otherwise false</returns>
+        Task<bool> ExistsAsync(TId id, CancellationToken ct);
+    }
+
+    /// <summary>
+    /// Represents the exists operation by an unique identifier
+    /// of <see cref="long"/> type.
+    /// </summary>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public interface IExistsById<TEntity> : IExistsById<TEntity, long>
+        where TEntity : class, IEntity<long>
+    {
+
+    }
+}
