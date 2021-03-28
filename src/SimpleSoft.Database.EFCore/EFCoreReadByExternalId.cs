160f8633300e7aa7bb35fbec3d846172b002bd1e
João Simões <gravity00@users.noreply.github.com>
2021-03-28 23:54:58 +0100

    implemented reads by external ids

---
+++ src/SimpleSoft.Database.EFCore/EFCoreReadByExternalId.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the read operation by an external unique identifier
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public class EFCoreReadByExternalId<TContext, TEntity, TId> : IReadByExternalId<TEntity, TId>
+        where TContext : DbContext
+        where TEntity : class, IEntity, IHaveExternalId<TId>
+        where TId : IEquatable<TId>
+    {
+        private readonly IQueryable<TEntity> _query;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context"></param>
+        public EFCoreReadByExternalId(
+            TContext context
+        )
+        {
+            _query = context.Set<TEntity>().AsNoTracking();
+        }
+
+        /// <inheritdoc />
+        public async Task<TEntity> ReadAsync(TId externalId, CancellationToken ct) =>
+            await _query.SingleOrDefaultAsync(e => e.ExternalId.Equals(externalId), ct);
+    }
+
+    /// <summary>
+    /// Represents the read operation by an external unique identifier
+    /// of <see cref="Guid"/> type.
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    public class EFCoreReadByExternalId<TContext, TEntity> : EFCoreReadByExternalId<TContext, TEntity, Guid>, IReadByExternalId<TEntity>
+        where TContext : DbContext
+        where TEntity : class, IEntity, IHaveExternalId
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context"></param>
+        public EFCoreReadByExternalId(TContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ src/SimpleSoft.Database.EFCore/EFCoreReadByExternalIdRange.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// Represents the read bulk operation by a collection of
+    /// external unique identifiers
+    /// </summary>
+    /// <typeparam name="TContext">The context type</typeparam>
+    /// <typeparam name="TEntity">The entity type</typeparam>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public class EFCoreReadByExternalIdRange<TContext, TEntity, TId> : IReadByExternalIdRange<TEntity, TId>
+        where TContext : DbContext
+        where TEntity : class, IEntity, IHaveExternalId<TId>
+        where TId : IEquatable<TId>
+    {
+        private readonly IQueryable<TEntity> _query;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="context"></param>
+        public EFCoreReadByExternalIdRange(
+            TContext context
+        )
+        {
+            _query = context.Set<TEntity>().AsNoTracking();
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<TEntity>> ReadAsync(IEnumerable<TId> externalIds, CancellationToken ct)
+        {
+            if (externalIds == null) throw new ArgumentNullException(nameof(externalIds));
+
+            return await _query.Where(e => externalIds.Contains(e.ExternalId)).ToListAsync(ct);
+        }
+    }
+}
