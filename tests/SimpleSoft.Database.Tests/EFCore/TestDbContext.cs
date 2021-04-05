343a7c070116ee88c4b2c8a4283d1e2fa6bb61ca
João Simões <gravity00@users.noreply.github.com>
2021-04-05 14:59:22 +0100

    test context and helper singletons

---
+++ tests/SimpleSoft.Database.Tests/EFCore/TestDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSoft.Database.EFCore
+{
+    public class TestDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) => builder
+            .UseSqlite(TestSingletons.ConnectionString);
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ExternalIdGuidEntity>(cfg =>
+            {
+                cfg.HasAlternateKey(e => e.ExternalId);
+
+                cfg.Property(e => e.ExternalId)
+                    .IsRequired();
+                cfg.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            builder.Entity<ExternalIdStringEntity>(cfg =>
+            {
+                cfg.HasAlternateKey(e => e.ExternalId);
+
+                cfg.Property(e => e.ExternalId)
+                    .IsRequired();
+                cfg.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            builder.Entity<IdLongEntity>(cfg =>
+            {
+                cfg.HasKey(e => e.Id);
+
+                cfg.Property(e => e.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+                cfg.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            builder.Entity<IdNoneEntity>(cfg =>
+            {
+                cfg.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            builder.Entity<IdStringEntity>(cfg =>
+            {
+                cfg.HasKey(e => e.Id);
+
+                cfg.Property(e => e.Id)
+                    .IsRequired()
+                    .HasMaxLength(32);
+                cfg.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+        }
+    }
+}
---
+++ tests/SimpleSoft.Database.Tests/EFCore/TestSingletons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Database.EFCore
+{
+    public static class TestSingletons
+    {
+        public static readonly string ConnectionString;
+        public static readonly IServiceProvider Provider;
+
+        static TestSingletons()
+        {
+            ConnectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = "SimpleSoftDatabaseEFCore",
+                Mode = SqliteOpenMode.Memory,
+                Cache = SqliteCacheMode.Private
+            }.ToString();
+
+            Provider = new ServiceCollection()
+                .AddDbContext<TestDbContext>()
+                .AddDbContextOperations<TestDbContext>(o =>
+                {
+                    o.NoTracking = true;
+                    o.AutoSaveChanges = true;
+                })
+                .BuildServiceProvider(true);
+        }
+    }
+}
