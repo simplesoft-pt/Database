e4f78b35e8eb9a2cfca7e3f88c1d1f7a5f7ba6bd
João Simões <gravity00@users.noreply.github.com>
2021-04-06 00:34:48 +0100

    split ContextContainer tests into multiple parcial files

---
+++ tests/SimpleSoft.Database.Tests/EFCore/ContextContainer/ContextContainerTests.AggregateAsync.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleSoft.Database.EFCore.ContextContainer
+{
+    public partial class ContextContainerTests
+    {
+        [Fact]
+        public async Task AggregateAsync_WithParameter_Aggregator_Null_Throws_ArgumentNullException()
+        {
+            // arrange
+            var container = CreateContainer();
+
+            // act
+            var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await container.AggregateAsync<object, object>(
+                    null,
+                    new object(),
+                    CancellationToken.None
+                )
+            );
+
+            // assert
+            Assert.NotNull(ex);
+            Assert.Equal("aggregator", ex.ParamName);
+        }
+
+        [Fact]
+        public async Task AggregateAsync_WithoutParameter_Aggregator_Null_Throws_ArgumentNullException()
+        {
+            // arrange
+            var container = CreateContainer();
+
+            // act
+            var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await container.AggregateAsync<object>(
+                    null,
+                    CancellationToken.None
+                )
+            );
+
+            // assert
+            Assert.NotNull(ex);
+            Assert.Equal("aggregator", ex.ParamName);
+        }
+
+        [Fact]
+        public async Task AggregateAsync_WithParameter_SameInstance()
+        {
+            // arrange
+            var container = CreateContainer();
+            var aggParam = new object();
+
+            // act
+            var result = await container.AggregateAsync((context, param, ct) => Task.FromResult(new
+            {
+                Context = context,
+                Param = param
+            }), aggParam, CancellationToken.None);
+
+            // assert
+            Assert.NotNull(result);
+
+            Assert.NotNull(result.Context);
+            Assert.Same(_dbContextFixture.Context, result.Context);
+            
+            Assert.NotNull(result.Param);
+            Assert.Same(aggParam, result.Param);
+        }
+
+        [Fact]
+        public async Task AggregateAsync_WithoutParameter_SameInstance()
+        {
+            // arrange
+            var container = CreateContainer();
+
+            // act
+            var result = await container.AggregateAsync((context, ct) => Task.FromResult(new
+            {
+                Context = context
+            }), CancellationToken.None);
+
+            // assert
+            Assert.NotNull(result);
+            
+            Assert.NotNull(result.Context);
+            Assert.Same(_dbContextFixture.Context, result.Context);
+        }
+    }
+}
--- tests/SimpleSoft.Database.Tests/EFCore/ContextContainer/ContextContainerTests.cs
+++ tests/SimpleSoft.Database.Tests/EFCore/ContextContainer/ContextContainerTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace SimpleSoft.Database.EFCore.ContextContainer
 {
-    public class ContextContainerTests : IClassFixture<DbContextFixture>
+    public partial class ContextContainerTests : IClassFixture<DbContextFixture>
     {
         private readonly DbContextFixture _dbContextFixture;
 
@@ -56,92 +56,13 @@ namespace SimpleSoft.Database.EFCore.ContextContainer
             Assert.NotNull(query);
         }
 
-        [Fact]
-        public async Task AggregateAsync_WithParameter_Aggregator_Null_Throws_ArgumentNullException()
-        {
-            // arrange
-            var container = CreateContainer();
-            var aggParam = new object();
-
-            // act
-            var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
-                await container.AggregateAsync<object, object>(
-                    null,
-                    aggParam,
-                    CancellationToken.None
-                )
-            );
-
-            // assert
-            Assert.NotNull(ex);
-            Assert.Equal("aggregator", ex.ParamName);
-        }
-
-        [Fact]
-        public async Task AggregateAsync_WithoutParameter_Aggregator_Null_Throws_ArgumentNullException()
-        {
-            // arrange
-            var container = CreateContainer();
-
-            // act
-            var ex = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
-                await container.AggregateAsync<object>(
-                    null,
-                    CancellationToken.None
-                )
-            );
-
-            // assert
-            Assert.NotNull(ex);
-            Assert.Equal("aggregator", ex.ParamName);
-        }
-
-        [Fact]
-        public async Task AggregateAsync_WithParameter_SameInstance()
-        {
-            // arrange
-            var container = CreateContainer();
-            var aggParam = new object();
-
-            // act
-            var result = await container.AggregateAsync((context, param, ct) => Task.FromResult(new
-            {
-                Context = context,
-                Param = param
-            }), aggParam, CancellationToken.None);
-
-            // assert
-            Assert.NotNull(result);
-
-            Assert.NotNull(result.Context);
-            Assert.Same(_dbContextFixture.Context, result.Context);
-            
-            Assert.NotNull(result.Param);
-            Assert.Same(aggParam, result.Param);
-        }
-
-        [Fact]
-        public async Task AggregateAsync_WithoutParameter_SameInstance()
-        {
-            // arrange
-            var container = CreateContainer();
-
-            // act
-            var result = await container.AggregateAsync((context, ct) => Task.FromResult(new
-            {
-                Context = context
-            }), CancellationToken.None);
-
-            // assert
-            Assert.NotNull(result);
-            
-            Assert.NotNull(result.Context);
-            Assert.Same(_dbContextFixture.Context, result.Context);
-        }
-
         private EFCoreContextContainer CreateContainer() => new EFCoreContextContainer(
             _dbContextFixture.Context,
-            Options.Create(new EFCoreContextContainerOptions())
+            Options.Create(new EFCoreContextContainerOptions
+            {
+                NoTracking = true,
+                AutoSaveChanges = true
+            })
         );
     }
 }
