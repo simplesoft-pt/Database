896a79f0a3d8520b0fa54e7f30c0cb2102880d81
João Simões <gravity00@users.noreply.github.com>
2021-04-05 14:59:32 +0100

    helper class fixtures

---
+++ tests/SimpleSoft.Database.Tests/EFCore/DbContextFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleSoft.Database.EFCore
+{
+    public class DbContextFixture : IDisposable
+    {
+        public DbContextFixture()
+        {
+            var context = new TestDbContext();
+
+            try
+            {
+                context.Database.EnsureCreated();
+            }
+            catch
+            {
+                context.Dispose();
+                throw;
+            }
+
+            Context = context;
+        }
+
+        public TestDbContext Context { get; }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
---
+++ tests/SimpleSoft.Database.Tests/EFCore/ServiceProviderFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Database.EFCore
+{
+    public class ServiceProviderFixture
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ServiceProviderFixture()
+        {
+            _scopeFactory = TestSingletons.Provider.GetRequiredService<IServiceScopeFactory>();
+        }
+
+        public async Task ExecuteAsync(Func<TestDbContext, Task> executor)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            await executor(scope.ServiceProvider.GetRequiredService<TestDbContext>());
+        }
+    }
+}
