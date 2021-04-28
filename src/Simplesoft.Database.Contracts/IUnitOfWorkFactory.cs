17edbecf91a2c8ecb03dc49b97573d2b7b83e551
Gonçalo Antunes <joao.g.antunes@rupeal.com>
2021-04-28 20:43:41 +0100

    Interfaces for Unit of Work Factory

---
+++ src/Simplesoft.Database.Contracts/IUnitOfWorkFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleSoft.Database
+{
+    /// <summary>
+	/// Interface representing the unit of work factory
+	/// </summary>
+    public interface IUnitOfWorkFactory
+    {
+        /// <summary>
+        /// Create a scoped unit of work
+		/// </summary>
+		/// <returns>An <see cref="IUnitOfWorkScoped"/></returns>
+        IUnitOfWorkScoped Create();
+    }
+}
