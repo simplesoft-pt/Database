787412b468e56f75fd0c7fa322188ec50592c763
Gonçalo Antunes <joao.g.antunes@rupeal.com>
2021-05-11 22:31:58 +0100

    NH Transaction, Uow and Factory

---
+++ src/SimpleSoft.Database.NH/NHTransaction.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Database
+{
+    /// <inheritdoc />
+    public class NHTransaction : ITransaction
+    {
+        private readonly NHSessionContainer _container;
+        private NHibernate.ITransaction _transaction;
+        private bool _disposed;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="container"></param>
+        public NHTransaction(NHSessionContainer container)
+        {
+            _container = container;
+        }
+
+        #region IDisposable
+
+        /// <inheritdoc />
+        ~NHTransaction() => Dispose(false);
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+
+#if NETSTANDARD2_1
+                DisposeAsync()
+                    .ConfigureAwait(false)
+                    .GetAwaiter()
+                    .GetResult();
+#else
+                _transaction?.Dispose();
+#endif
+            }
+
+            _transaction = null;
+            _disposed = true;
+        }
+
+#if NETSTANDARD2_1 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources asynchronously.
+        /// </summary>
+        /// <returns>A completed task of this operation</returns>
+        public virtual async ValueTask DisposeAsync()
+        {
+            if (_disposed)
+                return;
+
+            if (_transaction != null)
+                await _transaction.DisposeAsync();
+
+            _transaction = null;
+            _disposed = true;
+        }
+#endif
+
+        #endregion
+
+        /// <inheritdoc />
+        public async Task BeginAsync(CancellationToken ct)
+        {
+            if (_transaction != null)
+                throw new InvalidOperationException("Transaction already open.");
+
+            _transaction = await _container.BeginTransactionAsync(ct);
+        }
+
+        /// <inheritdoc />
+        public async Task CommitAsync(CancellationToken ct)
+        {
+            if (_transaction == null) throw new InvalidOperationException("Transaction must be open.");
+
+            await _container.FlushAsync(ct);
+
+            await _transaction.CommitAsync(ct);
+        }
+
+        /// <inheritdoc />
+        public Task RollbackAsync(CancellationToken ct)
+        {
+            if (_transaction == null) throw new InvalidOperationException("Transaction must be open.");
+
+            return _transaction.RollbackAsync(ct);
+        }
+    }
+}
---
+++ src/SimpleSoft.Database.NH/NHUnitOfWorkFactory.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SimpleSoft.Database
+{
+    /// <summary>
+    /// The implementation compatible with NHibernate for the Unit of Work Factory.
+    /// </summary>
+    public class NHUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="scopeFactory">The service scope factory to be used</param>
+        public NHUnitOfWorkFactory(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        /// <inheritdoc />
+        public IUnitOfWorkScoped Create() => new EFCoreUnitOfWorkScoped(_scopeFactory.CreateScope());
+
+        private class EFCoreUnitOfWorkScoped : NHUnitOfWork, IUnitOfWorkScoped
+        {
+            private IServiceScope _scope;
+
+            public EFCoreUnitOfWorkScoped(IServiceScope scope) : base(scope.ServiceProvider)
+            {
+                _scope = scope;
+            }
+
+            #region IDisposable
+
+            ~EFCoreUnitOfWorkScoped() => Dispose(false);
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if (disposing)
+                    _scope?.Dispose();
+
+                _scope = null;
+            }
+
+#if NETSTANDARD2_1
+
+            /// <inheritdoc />
+            public ValueTask DisposeAsync()
+            {
+                _scope?.Dispose();
+
+                Dispose(false);
+                GC.SuppressFinalize(this);
+
+                return default;
+            }
+
+#endif
+            #endregion
+        }
+    }
+}
